efc4334a1b06dade379d3367a1d948fcac574dda
UBUNTU <ubuntu@ubuntu>
2015-01-22 15:06:29 +0100

    Added rendering

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
 var express = require('express');
-var partials = require('express-partial');
 var router = express.Router();
 //NODEMAILER
 var nodemailer = require("nodemailer");
+var partials = require("express-partials");
+
+var app = express();
+
+
+app.use(partials());
 
 
 
@@ -14,23 +19,42 @@ var smtpTransport = nodemailer.createTransport("SMTP",{
     }
 });
 
+//HEADER
+app.render('../views/header.ejs', function (err, html) {
+
+    header = html;
+})
+    app.render('../views/confirmBody.ejs', function (err, html) {
+
+        body = html;
+    });
+
+    app.render('../views/footer.ejs', function (err, html) {
+
+        footer = html;
+        sendEmail();
+    });
+
+function sendEmail ()
+{
+
+    smtpTransport.sendMail({  //email options
+        from: "Sender Name <dsoft.tesla@gmail.com>", // sender address.  Must be the same as authenticated user if using Gmail.
+        to: "Receiver Name <michaall.k@gmail.com>", // receiver
+        subject: header, // include header.ejs
+        text: body+" "+footer// include confirmBody.ejs + include footer.ejs
+    }, function(error, response){  //callback
+        if(error){
+            console.log(error);
+        }else{
+            console.log("Message sent: " + response.message);
+        }
+
+        smtpTransport.close(); // shut down the connection pool, no more messages.  Comment this line out to continue sending emails.
+    });
 
 
-smtpTransport.sendMail({  //email options
-    from: "Sender Name <dsoft.tesla@gmail.com>", // sender address.  Must be the same as authenticated user if using Gmail.
-    to: "Receiver Name <michaall.k@gmail.com>", // receiver
-    subject: "TELO" , // include header.ejs
-    text: "BODY" // include confirmBody.ejs + include footer.ejs
-}, function(error, response){  //callback
-    if(error){
-        console.log(error);
-    }else{
-        console.log("Message sent: " + response.message);
-    }
-
-    smtpTransport.close(); // shut down the connection pool, no more messages.  Comment this line out to continue sending emails.
-});
-
+}
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express',nodemailer: nodemailer, partials: partials});
